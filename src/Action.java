1f5caed434ef798c40d89435aabd8ece9976d7cc
ut2mi <ut2mi2013@gmail.com>
2020-05-14 15:22:46 -0400

    Added classes for use: Action and Location

---
+++ src/Action.java
@@ -0,0 +1,11 @@
+public abstract class Action {
+    public Action(Location startLocation, Location endLocation, int numBeats) {
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
+        this.numBeats = numBeats;
+    }
+    Location startLocation;
+    Location endLocation;
+    int numBeats;
+
+}
---
+++ src/Location.java
@@ -0,0 +1,9 @@
+public class Location {
+    double yardsFromFifty = 0;
+    double yardsFromCenter = 0;
+    public Location(double yardsFromFifty, double yardsFromCenter) {
+        this.yardsFromFifty = yardsFromFifty;
+        this.yardsFromCenter = yardsFromCenter;
+    }
+
+}
